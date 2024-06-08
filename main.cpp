efd7eb0a6e597e516d3b24a7b9184503dac2dd0c
wu_virtual <1164928689@qq.com>
2024-06-08 17:02:48 +0800

    wu_virtual 实现sort函数

--- main.cpp
+++ main.cpp
@@ -3,6 +3,14 @@
 using namespace std;
 void sort(int arr[],int size)
 {
+    for(int i=0;i<size;i++){
+        for(int j=i+1;i<size-1-i;j++){
+            if(a[j]>a[j+1])
+            {
+                swap(a[j],a[j+1]);
+            }
+        }
+    }
 
 }
 
